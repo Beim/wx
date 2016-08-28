6c148ca78748bb801ef18c2dcebc7e42c180e64f
beim <beim2015@outlook.com>
2016-08-28 21:42:43 +0800

    1

--- routes/wechat.js
+++ routes/wechat.js
@@ -2,7 +2,6 @@ const router = require('koa-router')()
 const wechat = require('co-wechat')
 
 router.post('/', wechat('beim').middleware(function *() {
-    console.log(this)
     let message = this.weixin
     console.log('message: ', message)
     this.body = 'hahahaha'
