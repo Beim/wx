599f092c2f6192580903dcce79e91542bc15e7db
beim <beim2015@outlook.com>
2016-08-28 21:47:39 +0800

    1

--- routes/wechat.js
+++ routes/wechat.js
@@ -4,7 +4,15 @@ const wechat = require('co-wechat')
 router.post('/', wechat('beim').middleware(function *() {
     let message = this.weixin
     console.log('message: ', message)
-    this.body = 'hahahaha'
+    // this.body = 'hahahaha'
+    this.body = [
+        {
+            title: 'haha',
+            description: 'gfs',
+            picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+            url: 'https://www.baidu.com/'
+        }
+    ]
 }))
 
 module.exports = router
