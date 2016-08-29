0178421885fe0504eafda93bf6912630baea1659
beim <beim2015@outlook.com>
2016-08-29 09:39:27 +0800

    1

--- routes/wechat.js
+++ routes/wechat.js
@@ -1,19 +1,31 @@
 const router = require('koa-router')()
 const wechat = require('co-wechat')
 const fs = require('fs')
+const util  = require('./wechat/util.js')
 
 router.post('/', wechat('beim').middleware(function *() {
     let message = this.weixin
-    // console.log('message: ', message)
     fs.writeFile('./log', JSON.stringify(message, null, 4) + '\n\n', {encoding: 'utf8', flag: 'a'})
-    this.body = [
-        {
-            title: 'haha',
-            description: message,
-            picurl: 'http://img21.mtime.cn/pi/2011/04/04/161411.40182295_1000X1000.jpg',
-            url: 'https://www.baidu.com/'
+    if (message.MsgType === 'text') {
+        let arg = message.Content.split(' ')
+        let parser = util.dispatch(arg[0])
+        if (parser) {
+            arg.shift()
+            parser(arg).then((res) => {
+                this.body = res
+            })
+        } else {
+            this.body = 'Hello World@'
         }
-    ]
+    }
+    // this.body = [
+    //     {
+    //         title: 'haha',
+    //         description: message.Content,
+    //         picurl: 'http://img21.mtime.cn/pi/2011/04/04/161411.40182295_1000X1000.jpg',
+    //         url: 'https://www.baidu.com/'
+    //     }
+    // ]
 }))
 
 module.exports = router
