40025d58b3d1c01f1736b5c42064ce21269bcd36
beim <beim2015@outlook.com>
2016-08-29 10:39:23 +0800

    1

--- routes/wechat.js
+++ routes/wechat.js
@@ -5,29 +5,18 @@ const util  = require('./wechat/util.js')
 
 router.post('/', wechat('beim').middleware(function *() {
     let message = this.weixin
+    // 消息日志
     fs.writeFile('./log', JSON.stringify(message, null, 4) + '\n\n', {encoding: 'utf8', flag: 'a'})
     if (message.MsgType === 'text') {
         let arg = message.Content.split(' ')
         let parser = util.dispatch(arg[0])
-        console.log(parser)
         if (parser) {
             arg.shift()
-            // yield parser(arg).then((res) => {
-            //     this.body = res
-            // })
             this.body = yield parser(arg)
         } else {
             this.body = 'Hello World@'
         }
     }
-    // this.body = [
-    //     {
-    //         title: 'haha',
-    //         description: message.Content,
-    //         picurl: 'http://img21.mtime.cn/pi/2011/04/04/161411.40182295_1000X1000.jpg',
-    //         url: 'https://www.baidu.com/'
-    //     }
-    // ]
 }))
 
 module.exports = router
--- routes/wechat/url/index.js
+++ routes/wechat/url/index.js
@@ -1,6 +1,21 @@
+const path = require('path')
+const fs = require('fs')
+
+const DB_PATH = path.resolve(__dirname, '../../../dbs/urls.json')
+
 const url = (args) => {
     return new Promise((res, rej) => {
-        res('im url')
+        // e.g. url
+        if (args.length === 0) {
+            res('list all urls')
+        } else if (args.length === 1) {
+            let data = JSON.parse(fs.readFileSync(DB_PATH, 'utf8'))
+            let info = data[args[0]]
+            if (info) {
+                res(info.url)
+            }
+
+        }
     })
 }
 
