b443a23c5527fa46cbe49bbad760434d3bf5ebd4
beim <beim2015@outlook.com>
2016-09-07 07:28:27 +0800

    a

--- routes/wechat/mooc/index.js
+++ routes/wechat/mooc/index.js
@@ -1,20 +1,35 @@
+const fs = require('fs')
+const path = require('path')
+
+const DB = path.resolve(__dirname, '../../../dbs/moocUser.json')
+
 const test = (args) => {
     return new Promise((resolve, reject) => {
-        let body = [
-            {
-                title: 'test',
-                description: 'test des',
-                picurl: 'https://beim.github.io/images/hand_zheng.jpg',
-                url: 'https://www.baidu.com/'
-            },
-            {
-                title: 'test1',
-                description: 'test1 des',
-                picurl: 'https://beim.github.io/images/hand_zheng.jpg',
-                url: 'https://www.baidu.com/'
-            }
-        ]
-        resolve(body)
+        if (args.length === 0) {
+            resolve('请输入参数：')
+        } else if (args.length === 1) {
+            let users = fs.readFileSync(DB)
+            users = JSON.parse(users)
+            users.push(args[0])
+            fs.writeFileSync(DB, JSON.stringify(users, null, 4))
+            let body = '添加通知： ' + args[0]
+            resolve(body)
+        }
+        // let body = [
+        //     {
+        //         title: 'test',
+        //         description: 'test des',
+        //         picurl: 'https://beim.github.io/images/hand_zheng.jpg',
+        //         url: 'https://www.baidu.com/'
+        //     },
+        //     {
+        //         title: 'test1',
+        //         description: 'test1 des',
+        //         picurl: 'https://beim.github.io/images/hand_zheng.jpg',
+        //         url: 'https://www.baidu.com/'
+        //     }
+        // ]
+        // resolve(body)
     })
 }
 
