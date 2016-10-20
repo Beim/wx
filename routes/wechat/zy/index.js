4ba9bfcefb23f6a8936fa577566d2b9ce47bb7a6
beim <beim2015@outlook.com>
2016-10-21 01:58:28 +0800

    a

--- routes/wechat/zy/index.js
+++ routes/wechat/zy/index.js
@@ -7,6 +7,9 @@ const getIP = () => {
             let data = ''
             res.on('data', (c) => {data += c})
             res.on('end', () => {
+                for (let i in data) {
+                    data[i].index = i
+                }
                 resolve(JSON.parse(data))
             })
         })
