bd6fa421f796e7c30fa29fc16d743af655f6cc20
beim <beim2015@outlook.com>
2016-11-25 11:01:41 +0800

    a

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('koa-router')()
 
 router.get('/', function *(next) {
-    this.body = 'HELO Im beim\n email: beim2015@outlook.com'
+    this.body = 'HELO I\'m beim\n\nwechat: beiming945\n\nemail: beim2015@outlook.com\n\nblog: http://blog.beim.site/\n\ngithub: https://github.com/Beim\n\nshowURL service: http://beim.site/url/'
 })
 
 module.exports = router
